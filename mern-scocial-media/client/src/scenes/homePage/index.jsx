8eba78eccd9a84e7358c848b350a0ceb90d6217d
Prasad Vispute <visputepsd26@gmail.com>
2023-03-15 12:22:31 +0530

    navbar component

--- mern-scocial-media/client/src/scenes/homePage/index.jsx
+++ mern-scocial-media/client/src/scenes/homePage/index.jsx
@@ -1,7 +1,12 @@
-const HomePage = () =>{
-    return(
-        <div>HomePage</div>
-    );
-}
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
 
 export default HomePage;
