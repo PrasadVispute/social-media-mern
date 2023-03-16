c3f2392e297eb87547c86e3fd11e7b8433a37383
Prasad Vispute <visputepsd26@gmail.com>
2023-03-16 11:17:11 +0530

    Homepage userInfo widgets

--- mern-scocial-media/server/controllers/user.js
+++ mern-scocial-media/server/controllers/user.js
@@ -1,65 +1,61 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
 
 //getUSer
-export const getUser = async (req, res) =>{
-    try {
-        const { id } = req.params;
-        const user = await User.findById(id);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(404).json({error: error.message});
-    }
-}
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
 
 // gethis frnds
 export const getUserFriends = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.find(id);
-        const friends = Promise.all(
-            user.friends.map((id) => User.findById(id))
-        );
-        const formattedFriends = friends.map(
-            ({ _id, firstName, lastName, occupation, location, picturePath }) =>{
-                return { _id, firstName, lastName, occupation, location, picturePath }
-            }
-        );
+  try {
+    const { id } = req.params;
+    const user = await User.find(id);
+    const friends = Promise.all(user.friends.map((id) => User.findById(id)));
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
 
-        res.status(200).json(formattedFriends);
-    } catch (error) {
-        res.status(404).json({error: error.message});
-    }
-}
+    res.status(200).json(formattedFriends);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
 
 //add rem frnds
 export const addRemoveFriend = async (req, res) => {
-    try {
-        const { id, friendId } = req.params;
-        const user = await User.findById(id);
-        const friend = await User.findById(friendId);
-        
-        if(user.friends.includes(friendId)){
-            user.friends = user.friends.filter((id) => id != friendId);
-            friend.friends = friend.friends.filter((id)=> id != id);
-        }else{
-            user.frinds.push(friendId);
-            friend.friends.push(id);
-        }
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
 
-        await user.save();
-        await frind.save();
-
-        const friends = Promise.all(
-            user.friends.map((id) => User.findById(id))
-        );
-        const formattedFriends = friends.map(
-            ({ _id, firstName, lastName, occupation, location, picturePath }) =>{
-                return { _id, firstName, lastName, occupation, location, picturePath }
-            }
-        );
-
-        res.json(200).json(formattedFriends);
-    } catch (error) {
-        res.status(500).json({error: error.message});
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id != friendId);
+      friend.friends = friend.friends.filter((id) => id != id);
+    } else {
+      user.frinds.push(friendId);
+      friend.friends.push(id);
     }
-}
+
+    await user.save();
+    await frind.save();
+
+    const friends = Promise.all(user.friends.map((id) => User.findById(id)));
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.json(200).json(formattedFriends);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- mern-scocial-media/server/server.js
+++ mern-scocial-media/server/server.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import multer from 'multer';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 
-import { register } from './controllers/auth.js';
-import { createPost } from './controllers/posts.js';
-import { verifyToken } from './middleware/auth.js';
-import authRoutes from './routes/auth.js';
-import userRoutes from './routes/user.js';
-import postRoutes from './routes/posts.js';
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/posts.js";
 
 /*CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -25,23 +25,23 @@ const app = express();
 //middleware config
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser({limit:"30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(bodyParser({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /**FILE STG CONFIG */
 const storage = multer.diskStorage({
-    destination:function (req, file, cb){
-        cb(null, "public/assets");
-    },
-    filename:function(req, file, cb){
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer ({ storage });
+const upload = multer({ storage });
 
 /**ROUTES */
 app.use("/auth", authRoutes);
@@ -49,18 +49,21 @@ app.use("/user", userRoutes);
 app.use("/posts", postRoutes);
 
 /**ROUTES WITH FILES (images) */
-app.post('/auth/register', upload.single("picture"), register);
-app.post('/posts', upload.single("picture"), verifyToken, createPost);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", upload.single("picture"), verifyToken, createPost);
 
 /**MONGOOSE SETUP */
 const PORT = process.env.PORT || 6000;
-mongoose.connect(process.env.MONGO_URL,{
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`server started on port ${PORT}`)
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
     });
-}).catch((error)=>{
-    console.log(`${error} Did not connect!`)
-});
+  })
+  .catch((error) => {
+    console.log(`${error} Did not connect!`);
+  });
